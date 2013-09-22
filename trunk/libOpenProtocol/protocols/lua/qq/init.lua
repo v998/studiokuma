3cfbfcbe3e7e1e0a01c93a1711ae85660f43d38a
starkwong@gmail.com <starkwong@gmail.com@c285fad0-9dff-11de-b798-49b661d4457b>
2013-09-22 14:28:51 +0000

    Updated scripts

--- trunk/libOpenProtocol/protocols/lua/qq/init.lua
+++ trunk/libOpenProtocol/protocols/lua/qq/init.lua
@@ -1,8 +1,9 @@
 -- Generic startup script
 print('init.lua initiated with '.._VERSION)
-print('OpenProtocol instance=',OP_inst)
+print('MIMQQ4c/2.0 OpenProtocol instance=',OP_inst)
 
-if OP_ua==nil then OP_ua='Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET4.0C; .NET4.0E)' end
+if OP_ua==nil then OP_ua='Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.64 Safari/537.31' end
+
 if OP_fontname==nil then
 	OP_fontname='å®‹ä½“'
 	OP_fontsize=12
@@ -13,39 +14,58 @@ if OP_fontname==nil then
 end
 
 if OP_resume==nil then
-	dofile('lua/qq/func.lua')
+	-- dofile('lua/qq/comm.lua')
+	-- dofile('lua/qq/bit.lua')
+	-- dofile('lua/qq/func.lua') // TODO
 end
 
 if true then
 
 if OP_resume==nil then
+	OP_prestop=false
+	JSON=require('JSON')
+	dofile('lua/qq/api.lua')
+	dofile('lua/qq/get_user_friends2.lua')	
 	dofile('lua/qq/login.lua')
+	-- dofile('lua/qq/eqq.lua')
 else
 	OP_PollLoop()
 end
 
 else
--- [{"poll_type":"group_web_message","value":{"from_uin":3184390410,"group_code":2472696342,"group_type":1,"msg_id":20651,"msg_id2":518533,"msg_type":45,"reply_ip":176752291,"send_uin":3320937177,"to_uin":431533706,"ver":1,"xml":"<?xml version=\"1.0\" encoding=\"utf-8\"?><d><n t=\"h\" u=\"5089575\" i=\"6\" s=\"1.url.cn/qun/feeds/img/server/g16.png\"/><n t=\"t\" s=\"U+5201EU+06AB‰¹U+4E50\"/><n t=\"b\"/><n t=\"t\" s=\"”’‹àçŒ...-¬ŠãU+00E4U+00BCU+00A0\"/></d>"}}]
+-- [{"poll_type"."group_web_message","value".{"from_uin".3184390410,"group_code".2472696342,"group_type".1,"msg_id".20651,"msg_id2".518533,"msg_type".45,"reply_ip".176752291,"send_uin".3320937177,"to_uin".431533706,"ver".1,"xml"."<?xml version=\"1.0\" encoding=\"utf-8\"?><d><n t=\"h\" u=\"5089575\" i=\"6\" s=\"1.url.cn/qun/feeds/img/server/g16.png\"/><n t=\"t\" s=\"U+5201EU+06AB‰¹U+4E50\"/><n t=\"b\"/><n t=\"t\" s=\"”’‹àçŒ...-¬ŠãU+00E4U+00BCU+00A0\"/></d>"}}]
 print('Test Mode')
---local msg='[["font",{"size":9,"color":"000000","style":[0,0,0],"name":"\\u5B8B\\u4F53"}],["cface",{"name":"2B475214F097CF89EC98A2E159CFEA46.jpg","file_id":551750216,"key":"wYAqPaGD2mpm5V5d","server":"123.138.154.68:443"}]," \\u4F8B\\u5982\\u9019\\u6A23\\u5C31\\u6703\\u5728\\u6309OK\\u6642\\u986F\\u793A\\u4E00\\u500B\\u5C0D\\u8A71\\u6846 "]'
+--local msg='[["font",{"size".9,"color"."000000","style".[0,0,0],"name"."\\u5B8B\\u4F53"}],["cface",{"name"."2B475214F097CF89EC98A2E159CFEA46.jpg","file_id".551750216,"key"."wYAqPaGD2mpm5V5d","server"."123.138.154.68.443"}]," \\u4F8B\\u5982\\u9019\\u6A23\\u5C31\\u6703\\u5728\\u6309OK\\u6642\\u986F\\u793A\\u4E00\\u500B\\u5C0D\\u8A71\\u6846 "]'
 --print(msg)
---local j=JSON:decode(msg)
+--local j=JSON.decode(msg)
 --print(j[1][2].name)
 -- j[1][2].name=EncodeUTF8(j[1][2].name)
 -- j[3]=EncodeUTF8(j[3])
---local msg2=JSON:encode(j)
+--local msg2=JSON.encode(j)
 --print(msg2)
 
 --clientid='client_id'
 --psessionid='psession_id'
 --OP_SendMessage('1234567890\tä¸­æ–‡æ¸¬è©¦')
---CaptureQuotedParts("ptuiCB('0','0','http://webqq.qq.com/loginproxy.html?login2qq=1&webqq_type=10','0','xxxxx','x\\'xxxxxx');")
--- print(string.gsub("ptuiCB('0','0','http://webqq.qq.com/loginproxy.html?login2qq=1&webqq_type=10','0','xxxxx','x\'xxxxxx');","\\'","'"))
+--CaptureQuotedParts("ptuiCB('0','0','http.//webqq.qq.com/loginproxy.html?login2qq=1&webqq_type=10','0','xxxxx','x\\'xxxxxx');")
+-- print(string.gsub("ptuiCB('0','0','http.//webqq.qq.com/loginproxy.html?login2qq=1&webqq_type=10','0','xxxxx','x\'xxxxxx');","\\'","'"))
 
--- OP_UploadQunImage('D:\\Miranda-IM\\587D08324C50F0E9584ACA35CA4B13F1.JPG')
-verycode=OP_VeryCode('F:\\test\\verycode-2.jpg')
-print('verycode='..verycode)
+-- OP_UploadQunImage('D.\\Miranda-IM\\587D08324C50F0E9584ACA35CA4B13F1.JPG')
 
+-- ptwebqq=43e00ad9a8906134f18bd33030971b9b43ec7125db90bfb867e904f34c34c839
+-- uin=431533706
+-- hash=9692C09B
+
+-- local test=OP_Get("http://tgl.ath.cx/ggg",'http://webqq.qq.com/')
+-- local test=OP_Post('http://posttestserver.com/post.php?dump&dir=starkwong','http://webqq.qq.com/','r={"d":1,"c":2}');
+-- local test=OP_Post('http://posttestserver.com/post.php?dump&dir=starkwong','http://webqq.qq.com/','',-1,{from='control',f='EQQ.Model.ChatMsg.callbackSendPicGroup',vfwebqq='vfwebqq',custom_face='\tD:\\Miranda-IM\\cat64.jpg'})
+local test=OP_Get("http://www.sortchinese.com",'http://webqq.qq.com/')
+local test2=OP_GetCookie('PHPSESSID')
+
+print(test)
+print('phpsessid='..test2)
+
+-- print('P='..P('431533706','43e00ad9a8906134f18bd33030971b9b43ec7125db90bfb867e904f34c34c839'))
 print('End of test')
 dddd('ddd')
 OP_BadReply('Bla Bla')
--- trunk/libOpenProtocol/protocols/lua/qq/login.lua
+++ trunk/libOpenProtocol/protocols/lua/qq/login.lua
@@ -1,127 +1,259 @@
--- We should follow the protocol to access other pages first, but save some bandwidth now :)
+-- https://ui.ptlogin2.qq.com/cgi-bin/login?target=self&style=5&mibao_css=m_webqq&appid=1003903&enable_qlogin=0&no_verifyimg=1&s_url=http%3A%2F%2Fweb.qq.com%2Floginproxy.html&f_url=loginerroralert&strong_login=1&login_state=10&t=20121029001
+print('login.lua for comm.js v20130810')
 
-local url='http://check.ptlogin2.qq.com/check?uin='..OP_uin..'&appid=1003903&r='..math.random()
-local referer='http://ui.ptlogin2.qq.com/cgi-bin/login?target=self&style=5&mibao_css=m_webqq&appid=1003903&enable_qlogin=0&no_verifyimg=1&s_url=http%3A%2F%2Fwebqq.qq.com%2Floginproxy.html&f_url=loginerroralert&strong_login=1&login_state=10&t=20120920001'
---local referer='http://d.web2.qq.com/proxy.html?v=20110331002&callback=1&id=3'
+g_referer_top='https://ui.ptlogin2.qq.com/cgi-bin/login?daid=164&target=self&style=5&mibao_css=m_webqq&appid=1003903&enable_qlogin=0&no_verifyimg=1&s_url=http%3A%2F%2Fweb2.qq.com%2Floginproxy.html&f_url=loginerroralert&strong_login=1&login_state=10&t=20130723001'
+g_referer_web2='http://web2.qq.com/'
+g_referer_cgi='http://cgi.web2.qq.com/proxy.html?v=20110412001&callback=1&id=1'
 
 pgv_pvid=math.random(1000000000,9999999999)
 ssid="s"..math.random(1000000000,9999999999)
-cookie=''
--- cookie="pgv_pvid="..pgv_pvid.."; pgv_info=pgvReferrer=&ssid="..ssid.."; chkuin="..OP_uin
--- OP_Get('http://ui.ptlogin2.qq.com/cgi-bin/login?target=self&style=5&mibao_css=m_webqq&appid=1003903&enable_qlogin=0&no_verifyimg=1&s_url=http%3A%2F%2Fwebqq.qq.com%2Floginproxy.html&f_url=loginerroralert&strong_login=1&login_state=10&t=20120619001','http://webqq.qq.com/',cookie)
+-- pgv_pvid=math.random(10000,99999)..math.random(00000,99999)
+-- ssid="s"..math.random(10000,99999)..math.random(00000,99999)
 
-verycode=nil
-print('Initial login check...')
-local resp=OP_Get(url,referer,cookie)
-if string.find(resp,"ptui_checkVC(",1,true)==nil then
-	print('First time request failed for checkVC, try again')
-	resp=OP_Get(url,referer,cookie)
-end
--- ptui_checkVC('0','!53U','\x00\x00\x00\x00\x19\xb8\xae\x8a');
+-- Utilities
+encodeURIComponent=function(s)
+	return s
+end -- encodeURIComponent
 
-local ret
-
-if string.find(resp,"ptui_checkVC(",1,true)==nil then error('ERRRESP') end
-ret=CaptureQuotedParts(resp)
-
-if ret[1]=='0' then
-	verycode=ret[2]
+local loadvar=function(s,var)
+	local pos=s:find('var '..var)
+	local pos2
+	-- local ch
+	local term=';'
 	
-	print('Login '..OP_uin..' with verycode '..verycode)
-else
-	verycode=' '
-	while verycode==' ' do
-		print('Requesting verycode')
-		local url='http://captcha.qq.com/getimage?aid=1003903&r='..math.random()..'&uin='..OP_uin
-		local tempfile=OP_GetTempFile()
-		os.remove(tempfile)
-		tempfile=tempfile..'.jpg'
-		local img=OP_Get(url,referer)
-		if img:len() == 0 then
-			print('Verycode request failed, try again')
-			img=OP_Get(url,referer)
-		end
-		local fh=io.open(tempfile,"wb")
-		fh:write(img)
-		fh:close()
-		verycode=OP_VeryCode(tempfile)
-		os.remove(tempfile)
-	end
-end
-
-if verycode~=nil then
-	vc2=''
-	for c=3,string.len(ret[3]),4 do
-		vc2=vc2..string.char(tonumber(string.sub(ret[3],c,c+1),16))
-	end
-	local pwd_I=OP_MD5(OP_password)
-	local pwd_H=OP_MD5(pwd_I..vc2,true)
-	local pwd_G=OP_MD5(pwd_H..verycode,true)
-	
-	url='http://ptlogin2.qq.com/login?u='..OP_uin..'&p='..pwd_G..'&verifycode='..verycode..'&webqq_type=10&remember_uin=1&login2qq=1&aid=1003903&u1=http%3A%2F%2Fwebqq.qq.com%2Floginproxy.html%3Flogin2qq%3D1%26webqq_type%3D10&h=1&ptredirect=0&ptlang=2052&from_ui=1&pttype=1&dumy=&fp=loginerroralert&action=1-23-14531&mibao_css=m_webqq&t=1&g=1'
-	referer='http://ui.ptlogin2.qq.com/cgi-bin/login?target=self&style=5&mibao_css=m_webqq&appid=1003903&enable_qlogin=0&no_verifyimg=1&s_url=http%3A%2F%2Fwebqq.qq.com%2Floginproxy.html&f_url=loginerroralert&strong_login=1&login_state=10&t=20120619001'
-	
-	--print("url="..url)
-	--error('ERRASRT')
-	--cookie=cookie.."; ptui_loginuin="..OP_uin
-	--OP_Sleep(2000);
-	
-	print('Logging in...')
-	resp=OP_Get(url,referer,cookie)
-	
-	if string.find(resp,"ptuiCB(",1,true)==nil then
-		print('First time failed, try again')
-		resp=OP_Get(url,referer,cookie)
-	end
-	-- ptuiCB('0','0','http://webqq.qq.com/loginproxy.html?login2qq=1&webqq_type=10','0','...............', '^_^2');
-	
-	print('ASSERT: resp='..resp)
-	if string.find(resp,"ptuiCB(",1,true)==nil then error('ERRRESP'..resp) end
-	ret=CaptureQuotedParts(resp)
-	
-	if ret[1]=='0' then
-		nick=ret[6]
-		print('Welcome to MIMQQ4c, '..nick..'! :)\n')
-		
-		clientid=math.random(10000000,99999999)
-		
-		-- login2 is the only channel call that uses POST
-		local str='r='..JSON:encode({status=StatusMIM2Text(OP_status),ptwebqq=ptwebqq,passwd_sig='',clientid=clientid})
-		url='http://d.web2.qq.com/channel/login2'
-		referer='http://d.web2.qq.com/proxy.html?v=20110331002&callback=1&id=2'
-		print('Logging in channel...')
-		resp=OP_Post(url,referer,str)
-		
-		if resp:find('retcode')==nil or resp:byte(resp:len())~=125 then
-			print('login2: First time data failed, try again')
-			resp=OP_Post(url,referer,str)
-		end
-		
-		local j=JSON:decode(resp)
-		if (j.retcode==0) then
-			OP_LoginSuccess()
-			vfwebqq=j.result.vfwebqq
-			psessionid=j.result.psessionid
-			
-			print('Performing post login actions...')
-			API_GetSingleLongNick2(OP_uin)
-			API_GetQQLevel2(OP_uin)
-			API_GetFriendInfo2(OP_uin)
-			FACE_GetFace(OP_uin,1,0)
-			API_GetUserFriends2()
-			Channel_GetOnlineBuddies2()
-			API_GetGroupNameListMask2()
-			Channel_GetDiscuListNew2()
-			
-			OP_resume=true
-			OP_PollLoop()
-		else
-			error('ERRLOG2')
-		end
+	if pos==nil then
+		print('login.getvar('..var..'): Value not found!')
+		return ''
 	else
-		error('ERRLOGN'..ret[5])
+		pos=pos+4 -- +var:len()
+		
+		local pos2=s:find(term,pos)
+		local str=s:sub(pos,pos2-1)
+		
+		loadstring(str) -- s:sub(pos,pos2-1)
 	end
-else
-	print('No verycode given, cancel operation')
-	error('ERRNOVC')
+end
+
+local loadScript=function(url)
+	print ('login.loadScript: url='..url)
+	local data=OP_Get(url,referer)
+	print ('login.loadScript: data='..data)
+	data=data:gsub("%);",")")
+	loadstring(data)
+end
+
+local ptui_needVC=function(B,C)
+	-- cookie="pgv_pvid="..pgv_pvid.."; pgv_info=pgvReferrer=&ssid="..ssid.."; ptui_loginuin="..OP_uin.."; chkuin="..B
+	cookie="pgv_pvid="..pgv_pvid.."; pgv_info=pgvReferrer=&ssid="..ssid.."; chkuin="..B
+	referer=g_referer_top
+
+	local A = "https://ssl.ptlogin2."..g_domain.."/check?"
+	if g_regmaster == '2' then
+		A = "http://check.ptlogin2.function."..g_domain.."/check?regmaster=2&"
+	else
+		if g_regmaster == '3' then
+			A = "http://check.ptlogin2.crm2."..g_domain.."/check?regmaster=3&"
+		end
+	end
+	A=A.."uin="..B.."&appid="..C.."&js_ver="..g_pt_version.."&js_type=0&login_sig="..g_login_sig.."&u1=http%3A%2F%2Fwebqq.qq.com%2Floginproxy.html&r="..math.random()
+	loadScript(A)
+end
+
+local check=function()
+	g_uin=OP_uin
+	ptui_needVC(g_uin, g_appid)
+end
+
+-- Entry point
+
+local init=function()
+	print('login.init()')
+	local	data=OP_Get(g_referer_top,g_referer_web2);
+	loadvar(data,'g_version')
+	loadvar(data,'g_pt_version')
+	loadvar(data,'g_href')
+	loadvar(data,'g_appid')
+	loadvar(data,'g_daid')
+	loadvar(data,'g_regmaster')
+	loadvar(data,'g_login_sig')
+	loadvar(data,'g_lang')
+	loadvar(data,'g_domain')
+	loadvar(data,'g_mibao_css')
+	encodeURIComponent=nil
+	
+	print(' g_version='..g_version)
+	print(' g_pt_version='..g_pt_version)
+	print(' g_appid='..g_appid)
+	print(' g_regmaster='..g_regmaster)
+	print(' g_login_sig='..g_login_sig)
+	print(' g_lang='..g_lang)
+	print(' g_domain='..g_domain)
+	print(' g_mibao_css='..g_mibao_css)
+	
+	loadScript('https://ui.ptlogin2.qq.com/cgi-bin/ver',g_referer_top)
+	
+	login2token=nil
+	
+	check()
+	if verifycode~=nil and verifycode~='' then
+		print('Login '..g_uin..' with verifycode '..verifycode)
+		ajax_Submit()
+	else
+		error('ERRLOGNVerify Code Cancelled')
+	end
+end
+
+-- Delay loads
+
+local loadVC=function(A)
+	if A==true then
+		verifycode=' '
+		while verifycode==' ' do
+			print('Requesting verycode')
+			local E = "https://ssl.captcha."..g_domain.."/getimage?aid="..g_appid.."&r="..math.random().."&uin="..g_uin
+			local tempfile=OP_GetTempFile()
+			os.remove(tempfile)
+			tempfile=tempfile..'.jpg'
+			local img=OP_Get(E,referer)
+			if img:len() == 0 then
+				print('Verycode request failed, try again')
+				img=OP_Get(E,referer)
+			end
+			local fh=io.open(tempfile,"wb")
+			fh:write(img)
+			fh:close()
+			verifycode=OP_VeryCode(tempfile)
+			os.remove(tempfile)
+		end
+	end
+end
+
+ptuiV=function(v)
+	print('login.ptuiV: Web2 version '..v)
+end
+
+ptui_checkVC=function(B,E,C)
+	print('login.ptui_checkVC()')
+	
+	if C==nil then
+		check()
+		return
+	end
+
+	if C=="\x00\x00\x00\x00\x00\x00\x27\x10" then
+			g_uin = "0"
+			error('ERRLOGNInvalid UIN')
+			return
+	end
+	
+	checkVC_3 = C
+	
+	if B == "0" then
+		verifycode = E
+		loadVC(false)
+	else
+		if B == "1" then
+			loadVC(true)
+		end
+	end
+end
+
+local getSubmitUrl=function(K,U,P,V)
+	local E = true
+	local A = 'https://ssl.ptlogin2.'..g_domain..'/'..K..'?'
+	if g_regmaster=='2' then
+		A = 'http://ptlogin2.function.'..g_domain..'/'..K..'?regmaster=2&'
+	else
+		if g_regmaster=='3' then
+			A = 'http://ptlogin2.crm2.'..g_domain..'/'..K..'?regmaster=3&'
+		end
+	end
+	
+	A=A..'u='..U..'&p='..P..'&verifycode='..V
+	A=A..'&webqq_type=10&remember_uin=1&login2qq=1&aid='..g_appid
+	A=A..'&u1=http%3A%2F%2Fweb2.qq.com%2Floginproxy.html%3Flogin2qq%3D1%26webqq_type%3D10'
+	A=A..'&h=1&ptredirect=0&ptlang='..g_lang..'&daid=164&from_ui=1&pttype=1&dumy=&fp=loginerroralert&action=2-25-154876'
+	A=A..'&mibao_css='..g_mibao_css..'&t=1&g=1&js_type=0&js_ver='..g_pt_version..'&login_sig='..g_login_sig
+	return A
+end
+
+ajax_Submit=function()
+	print('login.ajax_Submit: checkVC_3='..checkVC_3)
+	
+	-- vc2=''
+	-- for c=3,string.len(checkVC_3),4 do
+	--	vc2=vc2..string.char(tonumber(string.sub(checkVC_3,c,c+1),16))
+	-- end
+	local pwd_I=OP_MD5(OP_password)
+	local pwd_H=OP_MD5(pwd_I..checkVC_3,true)
+	local pwd_G=OP_MD5(pwd_H..verifycode,true)
+
+	local A = getSubmitUrl('login',g_uin,pwd_G,verifycode)
+	-- pt.winName.set("login_param", encodeURIComponent(login_param));
+	loadScript(A)
+end
+
+function url_decode(str)
+  str = string.gsub (str, "+", " ")
+  str = string.gsub (str, "%%(%x%x)",
+      function(h) return string.char(tonumber(h,16)) end)
+  str = string.gsub (str, "\r\n", "\n")
+  return str
+end
+
+ptuiCB=function(J, K, B, H, C, A)
+	-- ptuiCB('3','0','','0','您输入的帐号或密码不正确，请重新输入。', '431533706');
+	-- ptuiCB('0','0','http://webqq.qq.com/loginproxy.html?login2qq=1&webqq_type=10','0','...............', '^_^2');
+	-- H=redirection window
+	-- J=65~67=qrlogin related
+	
+	if J == '0' then
+		-- Login success
+		print('login.ptuiCB: Welcome to MIMQQ4c, '..A..'! :)\n')
+		login2token=C
+		
+		cookie="pgv_pvid="..pgv_pvid.."; pgv_info=pgvReferrer=&ssid="..ssid.."; ptui_loginuin="..g_uin
+
+		if B ~= '' then
+			print('login.ptuiCB: Loading redirect '..B)
+			OP_Get(B,'https://ui.ptlogin2.qq.com/cgi-bin/login?daid=164&target=self&style=5&mibao_css=m_webqq&appid=1003903&enable_qlogin=0&no_verifyimg=1&s_url=http%3A%2F%2Fweb2.qq.com%2Floginproxy.html&f_url=loginerroralert&strong_login=1&login_state=10&t=20130723001')
+			
+			-- local pos=B:find('?u1=')
+			-- local url=url_decode(B:sub(pos+4))
+			-- print('#2: '.. url)
+			-- OP_Get(url,'https://ui.ptlogin2.qq.com/cgi-bin/login?daid=164&target=self&style=5&mibao_css=m_webqq&appid=1003903&enable_qlogin=0&no_verifyimg=1&s_url=http%3A%2F%2Fweb2.qq.com%2Floginproxy.html&f_url=loginerroralert&strong_login=1&login_state=10&t=20130723001')
+			
+		end
+		
+		OP_Post('http://cgi.web2.qq.com/keycgi/qqweb/newuac/get.do',g_referer_cgi,'r='..url_encode(JSON:encode({appid=50,itemlist={'width','height','defaultMode'}}))..'&uin='..g_uin)
+		
+		return
+	else
+		print('login.ptuiCB: Login Failed - '..C)
+		error('ERRLOGN'..C)
+	end
+end
+
+init()
+
+init=nil
+getvar=nil
+loadScript=nil
+ptui_needVC=nil
+check=nil
+loadVC=nil
+ptui_checkVC=nil
+ptuiV=nil
+getSubmitUrl=nil
+ajax_Submit=nil
+ptuiCB=nil
+checkVC_3=nil
+verifycode=nil
+OP_uin=nil
+OP_password=nil
+
+if login2token~=nil then
+	print('End of login.lua, handover to eqq.lua')
+	-- dofile('lua/qq/eqq.lua')
+	dofile('lua/qq/eqq_tmp.lua')
 end
